d4c91269470866763c8962387f9b65781873b7e4
Chris VZ <chris.vonzuben@gmail.com>
2014-12-10 21:01:45 -0500

    this may not be the best idea, kind of a waste of time

--- matricies.h
+++ matricies.h
@@ -6,8 +6,13 @@
 
         // row major matricies for use with opengl and glm. transormations are done
         // for opengl which is column major therefor everything is inversed
+template<typename T>
+class matm;
 
-template<typename T = float>
+template <typename T>
+void swap(matm<T>& r, matm<T>& l);
+
+template<typename T>
 class matm{
 
     private:
@@ -17,7 +22,7 @@ class matm{
 
     public:
 
-        matm(int _size = 16) :
+        matm(int _size = 0) :
             size(_size),
             mat(size ? new T[size] : 0)
             {}
@@ -43,12 +48,12 @@ class matm{
             delete[] mat;
         }
 
-        friend void swap(matm<T>& r, matm<T>& l);
+        friend void swap <T>(matm<T>& r, matm<T>& l);
 
-        matm& operator=(const matm& rhs);
-        matm& operator=(matm&& rhs);
-        matm operator*(const matm& rhs) const;
-        const matm& operator*=(const matm& rhs);
+        matm<T> operator=(const matm<T>& rhs);
+        matm<T> operator=(matm<T>&& rhs);
+        matm<T> operator*(const matm<T>& rhs) const;
+        const matm<T>& operator*=(const matm<T>& rhs);
         const T* operator*()const;
 
 
@@ -57,5 +62,69 @@ class matm{
         void zero();
 
         };
+
+template<typename T>
+void swap(matm<T>& r, matm<T>& l){
+    std::swap(r.size, l.size);
+    std::swap(r.mat, l.mat);
+}
+
+template<typename T>
+void matm<T>::identity(){ // should only be used on matrices that are thought of as square
+    zero();
+    for (int i = 0; i < size; i += std::sqrt(size) + 1)
+        mat[i] = 1;
+}
+
+template<typename T>
+void matm<T>::zero(){
+    std::fill(&mat[0], &mat[size], 0);
+}
+
+template<typename T>
+const T* matm<T>::operator*()const{
+    return mat;
+}
+
+template<typename T>
+matm<T> matm<T>::operator=(const matm<T>& rhs){
+    matm<T> tmp(rhs); // I do this so that I can have sudo copy swap assignment operator and move assignment
+    swap(*this, tmp);
+    return *this;
+}
+
+template<typename T>
+matm<T> matm<T>::operator=(matm<T>&& rhs){ // lets me avoid creating a copy for no reason
+    swap(*this, rhs);
+    return *this;
+}
+
+template<typename T>
+matm<T> matm<T>::operator*(const matm<T>& rhs) const{
+
+    matm<T> t(rhs.size);
+    t.zero();
+
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 4; col++) {
+            for (int inner = 0; inner < 4; inner++) {
+                t.mat[4 * row + col] += this->mat[4 * row + inner] * rhs.mat[4 * inner + col];
+            }
+        }
+    }
+
+    return t;
+}
+
+template<typename T>
+const matm<T>& matm<T>::operator*=(const matm<T>& rhs){
+
+    *this = *this * rhs;
+
+    return *this;
+}
+
+// the following functions are for 4 x 4 matricise only
+
 
 #endif
